d51d30e62d838a63481f76979169d950dada0649
Brendan Younger <github@brendanyounger.com>
2014-05-17 02:04:53 -0400

    Make session-store optional

--- src/ring_auth/middleware.clj
+++ src/ring_auth/middleware.clj
@@ -42,7 +42,7 @@
         auth-session
         (handle-timed-out-session! request session-store auth-session-id))
       (handle-insecure-session! request session-store auth-session-id))
-    (handle-unkown-session! request auth-session-id)))
+    (handle-unknown-session! request auth-session-id)))
 
 (defn make-auth-session
   "Create an authorization session with a given idle-timeout and max-age expressed in seconds"
@@ -71,48 +71,49 @@
     See the convenience function `make-auth-session` for making auth sessions.
 
   For general information about HTTPS routes and development practices, see the README."
-  [handler session-store]
-  (fn [request]
-    (let [auth-session-id   (get-in request [:cookies auth-cookie :value])
-          auth-session      (and auth-session-id (read-auth-session request session-store auth-session-id))
-          request           (if auth-session (assoc request :auth-session auth-session) request)
-          response          (handler request)
-          new-auth-session  (:auth-session response)]
-      (cond
-        (and (contains? response :auth-session) (nil? new-auth-session)) ;; delete auth session
-          (do
-            (store/delete-session session-store auth-session-id)
+  [handler & [options]]
+  (let [session-store (or (:store options) (memory/memory-store))]
+    (fn [request]
+      (let [auth-session-id   (get-in request [:cookies auth-cookie :value])
+            auth-session      (and auth-session-id (read-auth-session request session-store auth-session-id))
+            request           (if auth-session (assoc request :auth-session auth-session) request)
+            response          (handler request)
+            new-auth-session  (:auth-session response)]
+        (cond
+          (and (contains? response :auth-session) (nil? new-auth-session)) ;; delete auth session
+            (do
+              (store/delete-session session-store auth-session-id)
+              (assoc response :cookies
+                (merge (:cookies response)
+                       {auth-cookie {:value "" :max-age 0}})))
+
+          (and new-auth-session (not (secure? request))) ;; log error and don't set cookie
+            (do
+              (log/errorf "Refusing to set auth session on insecure request %s" (essential-request request))
+              (dissoc response :auth-session))
+
+          (and new-auth-session (secure? request)) ;; set the new auth-session, changing the id
+            (let [id (str (UUID/randomUUID))
+                  session (merge (make-auth-session (* 30 60) (* 24 60 60))
+                                 new-auth-session
+                                 {:last-access (coerce/to-long (time/now))})]
+              (if auth-session
+                (store/delete-session session-store auth-session-id))
+              (store/write-session session-store id session)
+              (assoc response :cookies
+                (merge (:cookies response)
+                       {auth-cookie {:value id :max-age (:max-age session) :secure true :http-only true}})))
+
+          (nil? auth-session-id) ;; no auth session, don't do anything
+            response
+
+          (nil? auth-session) ;; delete the old cookie
             (assoc response :cookies
               (merge (:cookies response)
-                     {auth-cookie {:value "" :max-age 0}})))
+                     {auth-cookie {:value "" :max-age 0}}))
 
-        (and new-auth-session (not (secure? request))) ;; log error and don't set cookie
-          (do
-            (log/errorf "Refusing to set auth session on insecure request %s" (essential-request request))
-            (dissoc response :auth-session))
-
-        (and new-auth-session (secure? request)) ;; set the new auth-session, changing the id
-          (let [id (str (UUID/randomUUID))
-                session (merge (make-auth-session (* 30 60) (* 24 60 60))
-                               new-auth-session
-                               {:last-access (coerce/to-long (time/now))})]
-            (if auth-session
-              (store/delete-session session-store auth-session-id))
-            (store/write-session session-store id session)
-            (assoc response :cookies
-              (merge (:cookies response)
-                     {auth-cookie {:value id :max-age (:max-age session) :secure true :http-only true}})))
-
-        (nil? auth-session-id) ;; no auth session, don't do anything
-          response
-
-        (nil? auth-session) ;; delete the old cookie
-          (assoc response :cookies
-            (merge (:cookies response)
-                   {auth-cookie {:value "" :max-age 0}}))
-
-        auth-session ;; update the auth session's last-access
-          (do
-            (if (time/after? (time/now) (time/plus (coerce/from-long (:last-access auth-session)) (time/seconds 30)))
-              (store/write-session session-store auth-session-id (assoc auth-session :last-access (coerce/to-long (time/now)))))
-            response)))))
+          auth-session ;; update the auth session's last-access
+            (do
+              (if (time/after? (time/now) (time/plus (coerce/from-long (:last-access auth-session)) (time/seconds 30)))
+                (store/write-session session-store auth-session-id (assoc auth-session :last-access (coerce/to-long (time/now)))))
+              response))))))
